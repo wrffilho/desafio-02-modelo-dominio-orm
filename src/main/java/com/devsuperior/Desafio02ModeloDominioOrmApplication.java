f967fafa314c9479c5873b9cc6ca85955814dd68
wrffilho <wanderlei.filho@prodam.gov.br>
2024-07-04 16:26:14 -0400

    first commit

---
+++ src/main/java/com/devsuperior/Desafio02ModeloDominioOrmApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio02ModeloDominioOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio02ModeloDominioOrmApplication.class, args);
+	}
+
+}
